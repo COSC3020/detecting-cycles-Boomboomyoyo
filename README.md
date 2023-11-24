6bae091c7e2b8a16cdfb5daa9dbded62d7859847
Joel Chambers <104284414+Boomboomyoyo@users.noreply.github.com>
2023-11-24 15:42:31 -0700

    Full Solution

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ exercises.
 
 What is the worst-case big $\Theta$ complexity of your implementation? Add your
 answer, including your reasoning, to this markdown file.
+
+# Response
+A good chunk of the I spent on this was trying to adapt test code to work instead finding a different set of test code to use. Eventually, I gave up, scrolled down, and it took 30 seconds to get the other set of test code on the site to work. I could've saved myself a lot of time.
+
+## Runtime Analysis
+Here are the steps my algorithm uses:
+1. Check for cycles starting from each node. Order $|V|$ time
+2. Iterate over each edge connected to start node. If there is an unvisited node connected to this node, repeat this step for it, then mark it visited.
+
+More or less, this comes down to the same asymptotic complexity as other exercises of $|V+E|$, but I do it from each start node, so that is a total complexity of $\Theta\left(|V^2+V*E|\right)$. I did it this way to account for unconnected graphs as well as connected graphs. I could make this more efficient with memoization, so I don't start in subgraphs that have already been checked for cycles, but I am ready to move on to other exercises, so I will leave this here, I think.
+
+## References
+Used the code from the second section of this as test code.
+https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
--- code.js
+++ code.js
@@ -1,3 +1,64 @@
-function hasCycle(graph) {
-    return false;
+function hasCycle(graph)
+{
+    flag = false
+    for(let i = 0; i < graph.length && !flag; i++)
+    {
+        flag = cycleChecker(graph, i)
+    }
+    return flag
 }
+
+function cycleChecker(graph, startNode, currentNode = startNode, visitedNodes = new Array(graph.length))
+{
+    if(visitedNodes[0] == undefined)
+    {
+        for(i = 0; i < visitedNodes.length; i++)
+        {
+            visitedNodes[i] = false
+        }
+    }
+    for(let i = 0; i < graph[currentNode].length; i++)
+    {
+        if(graph[currentNode][i] == startNode)
+        {
+            return true
+        }
+        if(!visitedNodes[graph[currentNode][i]])
+        {
+            visitedNodes[graph[currentNode][i]] = true
+            flag = cycleChecker(graph, startNode, graph[currentNode][i], visitedNodes)
+            if(flag)
+            {
+                return true
+            } else
+            {
+                visitedNodes[graph[currentNode][i]] = false
+            }
+        } else
+        {
+            return true
+        }
+    }
+    return false
+}
+
+function convertToAdjList(adjMatrix) {
+    dimensions = adjMatrix.length
+    adjList = []
+    for(i=0; i < dimensions; i++)
+    {
+        adjList.push([])
+        for(j=0; j < dimensions; j++)
+        {
+            if(adjMatrix[i][j] == 1)
+            {
+                adjList[i].push(j)
+            }
+        }
+    }
+    return adjList;
+}
+
+/*console.log(isCyclic(
+    [ [ 0, 0, 1 ], [ 0, 0, 0 ], [ 1, 1, 0 ] ]
+    ))*/
